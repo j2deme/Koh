6e190c4a169cbe676fe3244ec229be88f76fd29f
Jaime Delgado Meraz <j2deme@gmail.com>
2013-05-31 18:12:44 -0500

    Matrix generator creates random matrix changes and saves them to files

--- matrixgen.py
+++ matrixgen.py
@@ -1,2 +1,30 @@
-#!/usr/local/bin/python
-print('Hello World')
+#!/usr/bin/env python
+from numpy import *
+import sys
+import random
+
+
+# Saves matrix to file
+def saveToFile(m , i , len):
+	filename = "matrix%dx%d.%d.txt" % (len, len, i)
+	f = open(filename, "w")
+	f.write(str(m));
+	f.close()
+	return
+
+size = int(sys.argv[1])
+
+numbers = size * size
+
+matrix = arange(numbers).reshape((size,size))
+
+#print 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv)
+
+for i in xrange(0,size):
+	#print matrix
+	saveToFile(matrix, i, size)
+	x = random.randrange(0, size - 1)
+	y = random.randrange(0, size - 1)
+	r = random.randrange(1, numbers)
+	matrix[x][y] = r
